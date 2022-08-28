d687dd85cfbc359962c7cca3a7e65c55e0671e42
11AlexanderB <bezhenar93@internet.ru>
2022-08-28 19:23:25 +0300

    Палиндром

---
+++ 1zadanie/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int rev = 0;
+int n = num;
+
+while(n>0)
+{
+    int x = n%10;
+    rev = rev*10+x;
+    n = n/10;
+}
+if(rev == num) Console.WriteLine($"Число {num} является палиндромом");
+else Console.WriteLine($"Число {num} не является палиндромом");
