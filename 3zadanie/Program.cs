0967fa34fb32edaf90d0b3d15f4a0084d8c07905
11AlexanderB <bezhenar93@internet.ru>
2022-08-28 19:30:42 +0300

    Куб чисел

---
+++ 3zadanie/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+string res = "";
+while(i<=N)
+{
+    res = res + Math.Pow(i, 3) + ",";
+    i++;  
+}
+Console.WriteLine($"Куб чисел от 1 до {N}: {res}");
